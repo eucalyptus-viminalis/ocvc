0c22b9f39f2addc29aa22c6f1784527a66f7b948
eucalyptus-viminalis <aaqddf@icloud.com>
2024-03-23 09:55:41 +1100

    change to function

--- src/app/[fname]/frame/route.tsx
+++ src/app/[fname]/frame/route.tsx
@@ -1,60 +1,65 @@
 /* eslint-disable react/jsx-key */
+import { Frame, getFrameHtml } from 'frames.js';
 import {Button} from 'frames.js/core'
 import { createFrames } from "frames.js/next";
+import { NextRequest } from 'next/server';
 
-// export async function GET(
-//     req: NextRequest,
-//     { params }: { params: { fname: string } }
-// ) {
-//     const fname = params.fname;
-//     const image = req.nextUrl.toString() + '/image'
-//     const frame: Frame = {
-//         image,
-//         postUrl: req.nextUrl.toString(),
-//         version: 'vNext',
-//         // accepts,
-//         buttons: [
-//             {action:'post',label:'refresh'}
-//         ],
-//         imageAspectRatio: '1.91:1',
-//         // inputText,
-//         ogImage: image,
-//         // state,
-//     }
-//     const html = getFrameHtml(frame, {
-//         // htmlBody: ,
-//         // htmlHead,
-//         // og,
-//         title: fname + ' | ocvc*',
-//     })
-//     return new Response(html, {headers:{'content-type':'text/html'}});
-// }
-
-const frames = createFrames()
-const handleRequest = frames(async (ctx) => {
-
-    return {
-        image: ctx.url + '/image',
-        // kind: ,
-        // location,
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { fname: string } }
+) {
+    const fname = params.fname;
+    const image = req.nextUrl.toString() + '/image'
+    const frame: Frame = {
+        image,
+        postUrl: req.nextUrl.toString(),
+        version: 'vNext',
         accepts: [{
             id: 'xmtp',
             version: 'vNext'
         }],
-        // then,
         buttons: [
-            <Button action="post">refresh</Button>
+            {action:'post',label:'refresh'}
         ],
-        // headers: ,
-        // imageOptions: {
-
-        // },
+        imageAspectRatio: '1.91:1',
+        // inputText,
+        ogImage: image,
         // state,
-        // status,
-        // statusText,
-        // textInput,
     }
-})
+    const html = getFrameHtml(frame, {
+        // htmlBody: ,
+        // htmlHead,
+        // og,
+        title: fname + ' | ocvc*',
+    })
+    return new Response(html, {headers:{'content-type':'text/html'}});
+}
 
-export const GET = handleRequest
-export const POST = handleRequest
+// const frames = createFrames()
+// const handleRequest = frames(async (ctx) => {
+
+//     return {
+//         image: ctx.url + '/image',
+//         // kind: ,
+//         // location,
+//         accepts: [{
+//             id: 'xmtp',
+//             version: 'vNext'
+//         }],
+//         // then,
+//         buttons: [
+//             <Button action="post">refresh</Button>
+//         ],
+//         // headers: ,
+//         // imageOptions: {
+
+//         // },
+//         // state,
+//         // status,
+//         // statusText,
+//         // textInput,
+//     }
+// })
+
+// export const GET = handleRequest
+// export const POST = handleRequest
