aa26e98ab23c7277788504f40d11186576f2c29a
eucalyptus-viminalis <aaqddf@icloud.com>
2024-03-23 09:37:03 +1100

    other

--- src/app/[fname]/page.tsx
+++ src/app/[fname]/page.tsx
@@ -1,30 +1,46 @@
-import { Frame } from "frames.js"
-import { appConfig } from "../appConfig"
-import { FrameContainer } from "frames.js/next/server"
+import { Frame } from "frames.js";
+import { appConfig } from "../appConfig";
+import { FrameContainer } from "frames.js/next/server";
 import { fetchMetadata } from "frames.js/next";
- 
-export async function generateMetadata({params}: {params: {fname:string}}) {
-    const {fname} = params
-    console.log(fname)
-    const url = new URL('/' + fname.toString() + "/frame", process.env.VERCEL_URL || "http://localhost:3000")
-    console.log(url)
-    const meta = await fetchMetadata(url)
-    console.log(meta)
-  return {
-    title: "ocvc",
-    // provide full URL to your /frames endpoint
-    ...(await fetchMetadata(
-      new URL('/' + fname + "/frame", process.env.VERCEL_URL || "http://localhost:3000")
-    )),
-  };
+
+export async function generateMetadata({
+    params,
+}: {
+    params: { fname: string };
+}) {
+    const { fname } = params;
+    console.log(fname);
+    const url = new URL(
+        "/" + fname.toString() + "/frame",
+        process.env.VERCEL_URL || "http://localhost:3000"
+    );
+    console.log(url);
+    const meta = await fetchMetadata(url);
+    console.log(meta);
+    return {
+        title: "ocvc",
+        // provide full URL to your /frames endpoint
+        other: {
+            ...(await fetchMetadata(
+                new URL(
+                    "/" + fname + "/frame",
+                    process.env.VERCEL_URL || "http://localhost:3000"
+                )
+            )),
+        },
+    };
 }
 
-export default function UserRootPage({ params }: { params: { fname: string } }) {
-    const {fname} = params
+export default function UserRootPage({
+    params,
+}: {
+    params: { fname: string };
+}) {
+    const { fname } = params;
     // const image = appConfig.host + '/' + fname + '/image'
     return (
         <>
             <h1>{fname}</h1>
         </>
-    )
+    );
 }
