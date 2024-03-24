bfc629ea8ee9b66a473a2247a339687788759511
eucalyptus-viminalis <aaqddf@icloud.com>
2024-03-24 16:05:54 +1100

    fixing DATABASE_URL error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Login from "./Login";
 export default function Home() {
   // const checks = getData()
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4 text-xl">
         <h1 className="text-9xl">OCVC*</h1>
         <h1 className="text-5xl">Latest Checks</h1>
         <Login/>
